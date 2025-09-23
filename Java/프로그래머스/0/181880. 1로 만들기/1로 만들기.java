70256c0e04e4639abe33e55c86b84a03a44789ac
Cadentbeatle183 <63895102+Cadentbeatle183@users.noreply.github.com>
2025-09-23 18:29:51 +0900

    [level 0] Title: 1로 만들기, Time: 0.03 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int cnt=0;
+        for(int i:num_list){
+            while(i!=1){
+                if(i%2==0)
+                    i/=2;
+                else
+                    i=(i-1)/2;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
