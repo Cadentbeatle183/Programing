d73691e95390427247a08f2acbec980efe5b0e0f
Cadentbeatle183 <63895102+Cadentbeatle183@users.noreply.github.com>
2026-03-23 23:32:03 +0900

    [level 0] Title: 배열 만들기 6, Time: 44.19 ms, Memory: 120 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk=new ArrayList<>();
+        for(int i=0; i<arr.length; i++){
+            if(stk.size()==0)
+                stk.add(arr[i]);
+            else if(stk.get(stk.size()-1)==arr[i])
+                stk.remove(stk.size()-1);
+            else
+                stk.add(arr[i]);
+        }
+        if(stk.size()==0)
+            return new int[]{-1};
+        int[] answer=new int[stk.size()];
+        for(int i=0; i<answer.length; i++){
+            answer[i]=Integer.valueOf(stk.get(i));
+        }
+        return answer;
+    }
+}
