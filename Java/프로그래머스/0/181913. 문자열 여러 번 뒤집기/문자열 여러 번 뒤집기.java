4cb44821b6d8215d1cd257d6cb2b62f6d531bd4f
Cadentbeatle183 <63895102+Cadentbeatle183@users.noreply.github.com>
2026-04-04 11:30:11 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 19.62 ms, Memory: 81.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -2,8 +2,10 @@ class Solution {
     public String solution(String my_string, int[][] queries) {
         StringBuilder reverse = new StringBuilder();
         for(int i=0; i<queries.length; i++){
+            //뒤집을 부분 추출
             reverse.append(my_string.substring(queries[i][0], queries[i][1]+1));
             //System.out.println(answer);
+            //뒤집을 부분 전 + 뒤집을 부분 뒤집기 + 뒤집을 부분 후 = my_string
             my_string=my_string.substring(0, queries[i][0])+reverse.reverse()+my_string.substring(queries[i][1]+1);
             //System.out.println(my_string);
             reverse.setLength(0);
