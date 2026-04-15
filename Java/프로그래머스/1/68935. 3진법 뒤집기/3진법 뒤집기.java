be29244f2acb3e34b7cc7ff55df8c4ebf34c2629
Cadentbeatle183 <63895102+Cadentbeatle183@users.noreply.github.com>
2026-04-15 14:46:43 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.12 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        StringBuffer sb=new StringBuffer();
+        while(n>0){
+            sb.append(n%3);
+            n/=3;
+        }
+        return Integer.parseInt(sb.toString(), 3);
+    }
+}
