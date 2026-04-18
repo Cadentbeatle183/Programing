c9cfdaef9891792194250f2963fd9f02ab52ef89
Cadentbeatle183 <63895102+Cadentbeatle183@users.noreply.github.com>
2026-04-18 22:42:21 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 9.88 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        for(int i=0; i<s.length(); i++){
+            answer[i]=-1;
+            char ch=s.charAt(i);
+            for(int j=i-1; j>=0; j--){
+                if(ch==s.charAt(j)){
+                    answer[i]=i-j;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
