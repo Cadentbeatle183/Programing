63c5fe8c76066767f6d565552eb6bb5f62c7c663
Cadentbeatle183 <63895102+Cadentbeatle183@users.noreply.github.com>
2026-04-23 08:11:51 +0900

    [level 0] Title: 코드 처리하기, Time: 9.21 ms, Memory: 95.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String code) {
+        StringBuffer ret = new StringBuffer();
+        int mode=0;
+        for(int i=0; i<code.length(); i++){
+            char ch=code.charAt(i);
+            if(ch=='1'){
+                mode=mode==1?0:1;
+            }else if((i & 1)==1 && mode==1){
+                ret.append(ch);
+            }else if((i & 1)==0 && mode==0){
+                ret.append(ch);
+            }
+        }
+        if(ret.length()==0){
+            return "EMPTY";
+        }else{
+            return ret.toString();
+        }
+    }
+}
